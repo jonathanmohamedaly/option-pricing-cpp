b513a169770d0c6f1e2a1a36b66214bbc997cc42
Jonathan <mohamedalyjonathan@MacBook-Air-de-Jonathan.local>
2026-03-03 21:07:44 +0100

    plot of asian delta

--- docs/plot_asian_convergence.py
+++ docs/plot_asian_convergence.py
@@ -1,21 +1,52 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 
-mc_anti = pd.read_csv("convergence_mc_antithetic_asian.txt", sep=";")
-mc_control = pd.read_csv("convergence_mc_control_variate_asian.txt", sep=";")
-mc_control_geo = pd.read_csv("convergence_mc_control_variate_geometric.txt", sep=";")
+# -------------------------------
+# Convergence plot
+# -------------------------------
+mc = pd.read_csv("convergence_delta_asian_pathwise.txt", sep=";")
 
 plt.figure(figsize=(10,6))
-plt.plot(mc_anti['nSimulations'], mc_anti['Price'], label="Monte Carlo Antithetic", alpha=0.7)
-plt.plot(mc_control['nSimulations'], mc_control['Price'], label="Monte Carlo Control Variate", alpha=0.7)
-plt.plot(mc_control_geo['nSimulations'], mc_control_geo['Price'], label="Monte Carlo Control Variate Geo", alpha=0.7)
+plt.plot(mc['nSim'], mc['RelErr_PW_Ant_CV'], label="Pathwise Antithetic + CV", alpha=0.7)
 
 plt.xscale('log')
 plt.yscale('log')
 plt.xlabel("Number of simulations")
-plt.ylabel("Value of the option")
-plt.title(" Monte Carlo Antithetic vs Control Variate vs Control Variate Geo")
+plt.ylabel("Relative error")
+plt.title("Delta Convergence Control Variate")
 plt.legend()
 plt.grid(True, which="both", ls="--", alpha=0.5)
 
+plt.show()
+
+# -------------------------------
+# Histograms
+# -------------------------------
+df = pd.read_csv("delta_asian_histogram_pathwise.txt", sep=";")
+
+plt.figure(figsize=(8, 5))
+plt.hist(df["Delta_PW_Ant_CV"], bins=30, alpha=0.6, label="Pathwise Antithetic + CV")
+
+plt.xlabel("Delta value")
+plt.ylabel("Frequency")
+plt.title("Histogram of Delta estimates")
+plt.legend()
+plt.grid(True)
+plt.show()
+
+# -------------------------------
+# Delta vs Spot
+# -------------------------------
+df = pd.read_csv("delta_asian_vs_spot_pathwise.txt", sep=";")
+
+plt.figure(figsize=(8, 5))
+
+plt.plot(df["S0"], df["Delta_PW_Ant_CV"], label="Pathwise Antithetic + CV", linestyle="--", marker="^")
+plt.plot(df["S0"], df["Delta_Geo_Exact"], label="Delta Black-Scholes Geo", linewidth=2)
+
+plt.xlabel("S0")
+plt.ylabel("Delta")
+plt.title("Delta vs Spot: Pathwise methods vs Black-Scholes Geometric")
+plt.legend()
+plt.grid(True)
 plt.show()
